56f42db07f45875e562f0172304b1886c996b354
Mohammad Mahbubuzzaman <tachyon77@gmail.com>
2018-01-01 20:18:51 -0800

    Adds Nunit, Moq based unit testing project BusinessLogic.Tests

---
+++ BusinessLogic.Tests/AnswerManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using ProjectQ.DAL;
+using ProjectQ.Model;
+
+namespace ProjectQ.BusinessLogic.Tests
+{
+    [TestFixture]
+    public class AnswerManagerTests
+    {
+        [Test]
+        public void ShouldReturnAnswersForAQuestion()
+        {
+            var mockRepo = new Mock<IAnswerRepository>();
+            var mockUoW = new Mock<IUnitOfWork>();
+
+            var answers = new List<Answer>();
+            answers.Add(new Mock<Answer>().Object);
+
+            mockUoW
+                .Setup(x => x.AnswerRepository)
+                .Returns(mockRepo.Object);
+
+            var expected = Task
+                .FromResult<IEnumerable<Answer>>
+                (answers);
+            mockRepo
+                .Setup(x => x.GetForQuestion(1))
+                .Returns(expected);
+
+            IAnswerManager a = new AnswerManager(mockUoW.Object);
+            var actual = a.GetForQuestion(1).Result;
+
+            Assert.That(expected.Result, Is.EquivalentTo(actual));
+        }
+    }
+}
