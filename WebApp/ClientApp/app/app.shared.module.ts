5972181fecbfa597be3105cdfee9669485a6d27d
Mohammad Mahbubuzzaman <tachyon77@gmail.com>
2018-01-06 12:52:44 -0800

    Adds facebook based authentication!

--- WebApp/ClientApp/app/app.shared.module.ts
+++ WebApp/ClientApp/app/app.shared.module.ts
@@ -13,6 +13,7 @@ import { QuestionService } from './components/questions/questions.service';
 import { AnswerService } from './components/answers/answers.service';
 import { DisplayAnswersComponent } from './components/answers/displayanswers/displayanswers.component';
 import { AddAnswerFormComponent } from './components/answers/addanswer/addanswerform.component';
+import { FacebookLoginComponent } from './components/facebooklogin/facebooklogin.component'
 
 @NgModule({
     declarations: [
@@ -22,6 +23,7 @@ import { AddAnswerFormComponent } from './components/answers/addanswer/addanswer
         AddQuestionFormComponent,
         DisplayAnswersComponent,
         AddAnswerFormComponent,
+        FacebookLoginComponent,
         HomeComponent
     ],
     providers: [QuestionService, AnswerService],
---
+++ WebApp/ClientApp/app/components/facebooklogin/facebooklogin.component.ts
@@ -0,0 +1,42 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+
+declare const FB: any;
+
+@Component({
+    selector: 'facebook-login',
+    templateUrl: './facebooklogin.component.html'
+})
+
+export class FacebookLoginComponent implements OnInit {
+
+    constructor() {
+        console.log("FacebookLoginComponent: constructor()");
+    }
+
+    checkLoginState() {
+        FB.getLoginStatus(function (response: any) {
+            if (response.status === 'connected') {
+                // the user is logged in and has authenticated your
+                // app, and response.authResponse supplies
+                // the user's ID, a valid access token, a signed
+                // request, and the time the access token 
+                // and signed request each expire
+                var uid = response.authResponse.userID;
+                var accessToken = response.authResponse.accessToken;
+                console.log('uid = ' + uid);
+            } else if (response.status === 'not_authorized') {
+                // the user is logged in to Facebook, 
+                // but has not authenticated your app
+            } else {
+                // the user isn't logged in to Facebook.
+            }
+        });
+    }
+
+    ngOnInit() {
+        console.log('initing login');
+        
+    }
+}
