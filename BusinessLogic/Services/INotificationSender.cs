4867798246e1450641cacba762bf8594cfcf8267
Mohammad Mahbubuzzaman <tachyon77@gmail.com>
2018-03-10 08:38:16 -0800

    Removes unused tag

---
+++ BusinessLogic/Services/INotificationSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjectQ.Model;
+using System.Net.WebSockets;
+
+namespace ProjectQ.BusinessLogic.Services
+{
+    public interface INotificationSender
+    {
+        void Subscribe(string userId, WebSocket webSocket);
+        Task SendAsync(
+            IEnumerable<string> recipientUserIds, 
+            Notification notification);
+    }
+}
---
+++ BusinessLogic/Services/NotificationSender.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using ProjectQ.Model;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading;
+
+namespace ProjectQ.BusinessLogic.Services
+{
+    public class NotificationSender : INotificationSender
+    {
+        #region private fields
+        private Dictionary<string, List<WebSocket>> _websockets
+            = new Dictionary<string, List<WebSocket>>();
+        #endregion
+
+        void INotificationSender.Subscribe(string userId, WebSocket webSocket)
+        {
+            if (!_websockets.ContainsKey(userId))
+                _websockets[userId] = new List<WebSocket>();
+
+            _websockets[userId].Add(webSocket);
+        }
+
+        async Task INotificationSender.SendAsync(
+            IEnumerable<string> userIds, 
+            Notification notification)
+        {
+            foreach (var userId in userIds)
+            {
+                if(_websockets.ContainsKey(userId))
+                {
+                    foreach(var ws in _websockets[userId])
+                    {
+                        if (ws.State.Equals(WebSocketState.Open))
+                        {
+                            var data = JsonConvert.SerializeObject(notification);
+                            var encoded = Encoding.UTF8.GetBytes(data);
+                            var buffer = new ArraySegment<Byte>(
+                                encoded, 0, encoded.Length);
+
+                            await ws.SendAsync(
+                                buffer, WebSocketMessageType.Text,
+                                true,
+                                CancellationToken.None);
+
+                        }
+                        else
+                        {
+                            _websockets[userId].Remove(ws);
+                            if (!_websockets[userId].Any())
+                                _websockets.Remove(userId);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
