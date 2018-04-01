7704158b11294ad0996c15201dcfe3c883032a7c
Mohammad Mahbubuzzaman <tachyon77@gmail.com>
2018-04-01 11:03:44 -0700

    Adds sql table for employments and education. Updates view for profile page.

---
+++ Database/dbo/Tables/Employments.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employments]
+(
+	[Id] INT IDENTITY(1, 1) NOT NULL PRIMARY KEY, 
+	[UserId] NVARCHAR(450) NOT NULL,
+    [Position] NVARCHAR(200) NOT NULL, 
+    [Company] NVARCHAR(200) NOT NULL, 
+    [Start] DATETIME NULL, 
+    [End] DATETIME NULL, 
+    [IsCurrent] BIT NULL, 
+    CONSTRAINT [FK_Employments_AspNetUsers] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers]([Id])
+)
