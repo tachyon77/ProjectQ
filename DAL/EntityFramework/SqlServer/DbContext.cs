5140b9c5575486755f43f8741c8411f925d7fcec
Mahejabin Ruhul <moauna@outlook.com>
2018-04-14 00:07:12 -0700

    Adds ngx-bootstrap, updates local db name to just dot and makes first name nullable.

--- DAL/EntityFramework/SqlServer/DbContext.cs
+++ DAL/EntityFramework/SqlServer/DbContext.cs
@@ -10,7 +10,7 @@ namespace ProjectQ.DAL.EntityFramework.SqlServer
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 #if DEBUG
-            ConnectionString = "Data Source=DESKTOP-EC84EI0;Initial Catalog=ProjectQ;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
+            ConnectionString = "Data Source=.;Initial Catalog=ProjectQ;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
 #else
             ConnectionString = "Server=tcp:projectq-dbserver.database.windows.net,1433;Initial Catalog=ProjectQ;Persist Security Info=False;User ID=mahbub;Password=Zaqokm00;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 #endif
--- Database/dbo/Tables/AspNetUsers.sql
+++ Database/dbo/Tables/AspNetUsers.sql
@@ -14,7 +14,7 @@
     [SecurityStamp]        NVARCHAR (MAX)     NULL,
     [TwoFactorEnabled]     BIT                NOT NULL,
     [UserName]             NVARCHAR (256)     NULL,
-    [FirstName] NVARCHAR(200) NOT NULL, 
+    [FirstName] NVARCHAR(200) NULL, 
     [Introduction] NVARCHAR(MAX) NULL, 
     CONSTRAINT [PK_AspNetUsers] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
